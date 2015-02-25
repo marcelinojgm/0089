deee293f4c2c2c567a7f33c6e7e2454cfa6d8d48
MJGM <MJGM@example.com>
2015-02-25 12:02:35 +0100

    clase AgendaTelefonica con metodo enterNumber(String name, String number) para introducir un registro a la agenda y String lookupNumber(String name) para buscar un numero por el nombre

---
+++ AgendaTelefonica.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+/**
+ * Write a description of class AgendaTelefonica here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AgendaTelefonica
+{
+    // instance variables - replace the example below with your own
+    private HashMap<String, String> agenda;
+
+    /**
+     * Constructor for objects of class AgendaTelefonica
+     */
+    public AgendaTelefonica()
+    {
+        agenda = new HashMap<>();
+       
+    }
+
+    /**
+     * introduce un nuevo registro a la agenda
+     * @
+     * @
+     */
+    public void enterNumber(String name, String number){
+        agenda.put(name, number);
+    }
+
+    public String lookupNumber(String name){
+        return agenda.get(name);
+    }
+}
